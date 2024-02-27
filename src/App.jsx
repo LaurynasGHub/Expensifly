2cd1278b84e4b40b029be58c7013f36627d54fe0
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-02-27 23:05:09 +0200

    initial commit, start of the project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// components
+// import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
+// import Main from './components/Main/Main';
+// import MyCart from './components/MyCart/MyCart';
+
+import './App.scss';
+// import Favorites from './components/Favorites/Favorites';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navContainer">
+      <h1 className="title">Expensifly</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
