77e8ebb310c76dffd1246a390d26d962b4d7753d
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-03-02 01:48:51 +0200

    added navbar with responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// components
-// import Navbar from './components/Navbar/Navbar';
 import Navbar from './components/Navbar/Navbar';
-// import Main from './components/Main/Main';
-// import MyCart from './components/MyCart/MyCart';
 
 import './App.scss';
-// import Favorites from './components/Favorites/Favorites';
 
 function App() {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './navbar.scss';
+import { NavLink } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faReceipt,
+  faDollarSign,
+  faTag,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav className="navContainer">
-      <h1 className="title">Expensifly</h1>
+      <div className="navTitleContainer">
+        {/* <h1 className="navTitle">Expensifly</h1> */}
+        <NavLink to="/">
+          <h1 className="navTitle">Expensifly</h1>
+        </NavLink>
+        <p className="navTitlePar mb-0">Budgeting reimagined</p>
+      </div>
+      <ul className="mb-0">
+        <li>
+          <NavLink to="">Receipts</NavLink>
+          <FontAwesomeIcon icon={faReceipt} />
+        </li>
+        <li>
+          <NavLink to="">Expenses</NavLink>
+          <FontAwesomeIcon icon={faDollarSign} />
+        </li>
+        <li className="navbarCurrentPrices">
+          <NavLink to="">Current prices </NavLink>
+          <FontAwesomeIcon icon={faTag} />
+        </li>
+      </ul>
     </nav>
   );
 }
