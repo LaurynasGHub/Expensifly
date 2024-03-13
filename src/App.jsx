7b0d9b115d974447ccf5f314577f67e2a6768971
Laurynas Gailius <laurynasgailius1@gmail.com>
2024-03-13 16:19:43 +0200

    Fixed resizing align issues, changed color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
 import Receipts from './components/Receipts/Receipts';
 import CurrentPrices from './components/CurrentPrices/CurrentPrices';
-import Expenses from './components/CurrentPrices/Expenses/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Main />} />
+        {/* <Route path="/" element={<Main />} /> */}
         <Route path="/receipts" element={<Receipts />} />
         <Route path="/expenses" element={<Expenses />} />
         <Route path="/current-prices" element={<CurrentPrices />} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,21 +14,21 @@ function Navbar() {
   return (
     <nav className="navContainer">
       <div className="navTitleContainer">
-        <NavLink to="/">
-          <h1 className="navTitle">Expensifly</h1>
-        </NavLink>
+        {/* <NavLink to="/"> */}
+        <h1 className="navTitle">Expensifly</h1>
+        {/* </NavLink> */}
         <p className="navTitlePar mb-0">Budgeting reimagined</p>
       </div>
       <ul className="mb-0">
         <li>
-          <NavLink to="/receipts">Receipts</NavLink>
+          <NavLink to="/receipts">Receipts/ expenses</NavLink>
           <FontAwesomeIcon icon={faReceipt} />
         </li>
         <li>
-          <NavLink to="/expenses">Expenses</NavLink>
+          <NavLink to="/expenses">Budget</NavLink>
           <FontAwesomeIcon icon={faDollarSign} />
         </li>
-        <li className="navbarCurrentPrices">
+        <li>
           <NavLink to="/current-prices">Current prices </NavLink>
           <FontAwesomeIcon icon={faTag} />
         </li>
