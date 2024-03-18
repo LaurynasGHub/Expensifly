12c26b5749969850f10386046b577f182fe5cf53
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-03-18 14:59:30 +0200

    trying to make gmail API work

--- public/js/utils/gmailAPI/getAllEmails.js
+++ public/js/utils/gmailAPI/getAllEmails.js
@@ -1,2 +1,3 @@
 "use strict";
+const google = require('googleapis');
 //# sourceMappingURL=getAllEmails.js.map
--- src/utils/gmailAPI/getAllEmails.ts
+++
@@ -1,1 +0,0 @@
-//this function should get all the emails in an inbox and return a json object with all the necessary data using gmail API
