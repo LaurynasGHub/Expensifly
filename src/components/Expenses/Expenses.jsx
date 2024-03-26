34f20f98558d59673e8a8a746d8da37f16429a1e
Laurynas Gailius <laurynasgailius1@gmail.com>
2024-03-26 15:27:41 +0200

    Deleted AddExpenseButton

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -7,7 +7,6 @@ import mockReceiptData from '../../mockReceiptData';
 
 //components
 import ExpenseCard from '../ExpenseCard/ExpenseCard';
-import AddButton from '../AddExpenseButton/AddExpenseButton';
 
 const mockData = mockReceiptData;
 
@@ -25,10 +24,10 @@ function Receipts() {
       <div className="filterOptions p-3">
         <div className="filterByMonth">
           <select
-            class="form-select form-select-sm shadow-none form-control"
+            className="form-select form-select-sm shadow-none form-control"
             aria-label=".form-select-sm example"
           >
-            <option selected>Select month</option>
+            <option>Select month</option>
             <option value="1">Jan</option>
             <option value="2">Feb</option>
             <option value="3">March</option>
@@ -36,10 +35,10 @@ function Receipts() {
         </div>
         <div className="filterByYear ps-3">
           <select
-            class="form-select form-select-sm shadow-none form-control"
+            className="form-select form-select-sm shadow-none form-control"
             aria-label=".form-select-sm example"
           >
-            <option selected>Select year</option>
+            <option>Select year</option>
             <option value="1">2023</option>
             <option value="2">2024</option>
           </select>
